5a12dc44a0623961195266a896f74963d89f74c6
S Kyrikakis <kirikakis@mgail.com>
2024-11-23 20:15:57 +0000

    remove start cmd add startup delay to microros

--- start_microros.sh
+++ start_microros.sh
@@ -2,4 +2,5 @@ export ROS_DOMAIN_ID=20  && \
 source /opt/ros/humble/setup.bash && \
 source /workspaces/Robot_Pi_4WD_ROS/install/setup.bash && \
 source /uros_ws/install/setup.bash && \
+sleep 10 && \
 ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB_ROS -b 921600 -v4
