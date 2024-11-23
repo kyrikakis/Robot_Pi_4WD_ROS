c91e4617f8231f9d97d85beba7d73be8c8ac2b22
S Kyrikakis <kirikakis@mgail.com>
2024-11-23 11:56:22 +0000

    fix camera start script

--- start_camera.sh
+++ start_camera.sh
@@ -8,5 +8,5 @@ trap cleanup INT
 
 export ROS_DOMAIN_ID=20  && \
 source /opt/ros/humble/setup.bash && \
-source /workspaces/Robot_Pi_4WD_ROS/install/setup.bash && \
+source /colcon_ws/install/setup.bash && \
 ros2 run camera_ros camera_node --ros-args -p camera:=0 -p width:=1024 -p height:=576 -p role:=video -p format:=BGR888 -p FrameDurationLimits:="[50000,50000]"
