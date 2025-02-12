8efa4412708aba12183083bea4a0ec76c44c4704
Stefanos Kyrikakis <kirikakis@gmail.com>
2025-02-12 18:56:47 +0000

    refactor eyes

--- start_eyes.py
+++ start_eyes.py
@@ -2,6 +2,11 @@ import board
 import busio
 from adafruit_ht16k33 import matrix
 import time
+import rclpy
+from rclpy.node import Node
+from threading import Thread
+from std_msgs.msg import String
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
 class MATRIX_MODE:
     NONE = "1"
@@ -11,7 +16,8 @@ class MATRIX_MODE:
     RIGHT = "5"
     LEFT = "6"
     BLINK = "7"
-    RANDOM = "8"
+    OPEN = "8"
+    RANDOM = "9"
 
 eyeSmile = [
     [False, False, False, False, False, False, False, False],
@@ -35,14 +41,15 @@ eyeShut = [
 ]
 eyeOpen = [
     [False, False, False, False, False, False, False, False],
+    [False, False, False, False, False, False, False, False],
+    [False, False, False, True, True, False, False, False],
     [False, False, True, True, True, True, False, False],
-    [False, True, False, False, False, False, True, False],
-    [False, True, False, False, False, False, True, False],
-    [False, True, False, False, False, False, True, False],
-    [False, False, True, True, True, True, True, False],
+    [False, False, False, True, True, False, False, False],
+    [False, False, False, False, False, False, False, False],
     [False, False, False, False, False, False, False, False],
     [False, False, False, False, False, False, False, False]
 ]
+
 eyeOpenFill = [
     [False, False, False, False, False, False, False, False],
     [False, False, True, True, True, True, False, False],
@@ -54,13 +61,31 @@ eyeOpenFill = [
     [False, False, False, False, False, False, False, False]
 ]
 
-class CustomLEDMatrixController:
+class CustomLEDMatrixController(Node):
     animation = MATRIX_MODE.NONE
     
     def __init__(self, i2c_address=0x71):
+        super().__init__('eyes')
+        self.subscription = self.create_subscription(
+            String,
+            '/eyes',
+            self.callback,
+            QoSProfile(reliability=ReliabilityPolicy.RELIABLE, history=HistoryPolicy.KEEP_LAST, depth=1))
         # Initialize I2C bus and HT16K33 LED matrix with the specified address
         i2c = busio.I2C(board.SCL, board.SDA)
         self.matrix = matrix.Matrix16x8(i2c, address=i2c_address)
+
+    def eyes_blink(self):
+        self.clear_display()
+        while self.animation==MATRIX_MODE.BLINK:
+            # Fill the display with the eyeSmile data for both left and right
+            self.fill_display(eyeOpenFill)
+            # Update the physical display
+            self.show_display()
+            # Fill the display with the eyeSmile data for both left and right
+            self.fill_display(eyeOpen)
+            # Update the physical display
+            self.show_display()
     
     def eyes_smile(self):
         self.clear_display()
@@ -69,25 +94,44 @@ class CustomLEDMatrixController:
             self.fill_display(eyeSmile)
             # Update the physical display
             self.show_display()
-            time.sleep(4)
-            # Fill the display with the eyeSmile data for both left and right
-            self.fill_display(eyeShut)
-            # Update the physical display
-            self.show_display()
-            time.sleep(1)
 
-    def eyes_blink(self):
+            start_time = time.time()
+            while time.time() - start_time < 3.0: #sec
+                if self.animation!=MATRIX_MODE.HAPPY:
+                    break
+                time.sleep(0.1)
+                
+            # Fill the display with the eyeSmile data for both left and right
+            if self.animation==MATRIX_MODE.HAPPY:
+                self.fill_display(eyeShut)
+            # Update the physical display
+            if self.animation==MATRIX_MODE.HAPPY:
+                self.show_display()
+                
+            start_time = time.time()
+            while time.time() - start_time < 1.0: #sec
+                if self.animation!=MATRIX_MODE.HAPPY:
+                    break
+                time.sleep(0.1)
+
+    def eyes_open(self):
         self.clear_display()
-        while self.animation==MATRIX_MODE.BLINK:
-            # Fill the display with the eyeSmile data for both left and right
-            self.fill_display(eyeOpen)
-            # Update the physical display
-            self.show_display()
-            # Fill the display with the eyeSmile data for both left and right
-            self.fill_display(eyeOpenFill)
-            # Update the physical display
-            self.show_display()
+        open = False
+        while self.animation==MATRIX_MODE.OPEN:
+            if open == False:
+                self.fill_display(eyeOpen)
+                self.show_display()
+                open = True
+            time.sleep(0.1)
 
+    def eyes_movement(self):
+        while rclpy.ok():
+            match self.animation:
+                case MATRIX_MODE.HAPPY:
+                    self.eyes_smile()
+                case MATRIX_MODE.OPEN:
+                    self.eyes_open()
+            
     def fill_display(self, data):
         """
         Fill the display with data (16x8 list of True/False values).
@@ -109,15 +153,28 @@ class CustomLEDMatrixController:
         """
         self.matrix.show()
 
+    def callback(self, msg):
+        if msg.data == 'happy':
+          self.animation = MATRIX_MODE.HAPPY
+        elif msg.data == 'open':
+          self.animation = MATRIX_MODE.OPEN
+          
+def main(args=None):
+    rclpy.init(args=args)
+
+    display = CustomLEDMatrixController()
+    display.animation = MATRIX_MODE.HAPPY
+    thread = Thread(target = CustomLEDMatrixController.eyes_movement, args=(display,))
+    thread.start()
+
+    rclpy.spin(display)
+    thread.join()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    display.destroy_node()
+    rclpy.shutdown()
 
 # Main program logic follows:
 if __name__ == '__main__':
-    print("Running main. After that, you can press ctrl-C to end the program.")
-    display=CustomLEDMatrixController()
-    display.animation = MATRIX_MODE.HAPPY
-    while True:
-        try :
-            display.eyes_smile()
-        except KeyboardInterrupt:
-            print ("\nEnd of program")
-            break
+    main()
