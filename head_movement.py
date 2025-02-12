f8c5d24433baf8aa479269b138b7954bd44c03de
Stefanos Kyrikakis <kirikakis@gmail.com>
2025-02-12 18:56:30 +0000

    fix head movement orientation

--- head_movement.py
+++ head_movement.py
@@ -1,21 +1,21 @@
-from threading import Thread
 import rclpy
+import math
 from rclpy.node import Node
+from threading import Thread
 
-from example_interfaces.msg import Int32MultiArray
 from std_msgs.msg import Int32
 
 import numpy as np
-from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+from tf2_ros.transform_broadcaster import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
+import time
 
-SEND_INTERVAL_SEC = 0.25
-MIN_RECEIVED_INTERVAL_SEC = 0.25
-Y_MIN = -19
-Y_MAX = 36
+SEND_INTERVAL_SEC = 0.20
+Y_MIN = -36
+Y_MAX = 19
 Y_OFFSET = -56
-Z_MIN = -38
-Z_MAX = 40
+Z_MIN = -40
+Z_MAX = 38
 Z_OFFSET = 0
 
 def cap_to_y_limits(y_rotation):
@@ -33,56 +33,103 @@ def cap_to_z_limits(z_rotation):
         return Z_MAX
     else:
         return z_rotation
+
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
+
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
+
+    return q
     
 class HeadMovement(Node):
 
     def __init__(self):
         super().__init__('head_movement')
-        self.subscription = self.create_subscription(
-            Int32MultiArray,
-            '/head/yz_rotation',
-            self.listener_callback,
+        self.y_subscription = self.create_subscription(
+            Int32,
+            '/head/y_rotation',
+            self.y_callback,
             10)
-        self.subscription  # prevent unused variable warning
-        self.y_publisher = self.create_publisher(Int32, 'servo_s2', 50)
-        self.z_publisher = self.create_publisher(Int32, 'servo_s1', 50)
-        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+        self.z_subscription = self.create_subscription(
+            Int32,
+            '/head/z_rotation',
+            self.z_callback,
+            10)
+        self.y_publisher = self.create_publisher(Int32, 'servo_s2', 10)
+        self.z_publisher = self.create_publisher(Int32, 'servo_s1', 10)
+        self.tf_broadcaster = TransformBroadcaster(self)
 
         self.y_rotation = 0
         self.z_rotation = 0
+        self.move_y(self.y_rotation)
+        self.move_z(self.z_rotation)
+        self.move_y(self.y_rotation)
+        self.move_z(self.z_rotation)
 
-    def listener_callback(self, msg):
-        y_rotation = msg.data[0]
-        z_rotation = msg.data[1]
+    def y_callback(self, msg):
+        y_rotation = msg.data
         y_rotation = cap_to_y_limits(y_rotation)
-        z_rotation = cap_to_z_limits(z_rotation)
-        self.move_head(y_rotation, z_rotation)
         self.y_rotation = y_rotation
-        self.z_rotation = z_rotation
-        self.publish_transform()
+        self.move_y(y_rotation)
 
-    def move_head(self, y_rotation, z_rotation):
+    def z_callback(self, msg):
+        z_rotation = msg.data
+        z_rotation = cap_to_z_limits(z_rotation)
+        self.z_rotation = z_rotation
+        self.move_z(z_rotation)
+
+    def move_y(self, y_rotation):
         y_msg = Int32()
-        z_msg = Int32()
-        y_msg.data = y_rotation + Y_OFFSET
-        z_msg.data = z_rotation + Z_OFFSET
+        y_msg.data = y_rotation*-1 + Y_OFFSET #servo has the rotation sign reversed thus the *-1
         self.y_publisher.publish(y_msg)
+
+    def move_z(self, z_rotation):
+        z_msg = Int32()
+        z_msg.data = z_rotation*-1 + Z_OFFSET #servo has the rotation sign reversed thus the *-1
         self.z_publisher.publish(z_msg)
 
     def publish_transform(self):
-        t = TransformStamped()
-        t.transform.rotation.y = np.radians(self.y_rotation)
-        t.transform.rotation.z = np.radians(self.z_rotation)
-        self.tf_static_broadcaster.sendTransform(t)
-            
+        while(True):
+            time.sleep(SEND_INTERVAL_SEC)
+            t = TransformStamped()
+            t.header.stamp = self.get_clock().now().to_msg()
+            t.header.frame_id = 'base_link'
+            t.child_frame_id = 'depth_frame'
+            t.transform.translation.x = 0.10
+            t.transform.translation.y = 0.05
+            t.transform.translation.z = 0.06
+            quat = quaternion_from_euler(0.0, np.radians(self.y_rotation), np.radians(self.z_rotation))
+            t.transform.rotation.x = quat[0]
+            t.transform.rotation.y = quat[1]
+            t.transform.rotation.z = quat[2]
+            t.transform.rotation.w = quat[3]
+            self.tf_broadcaster.sendTransform(t)
 
 def main(args=None):
     rclpy.init(args=args)
 
     head_movement = HeadMovement()
+    thread = Thread(target = HeadMovement.publish_transform, args=(head_movement,))
+    thread.start()
 
     rclpy.spin(head_movement)
-
+    thread.join()
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
